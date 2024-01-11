d6eabae7b5f67932cccd75aa3e0891f52a58d15e
joaoboscochagaspanzera <joaopanzera@hotmail.com>
2024-01-11 18:57:34 -0300

    Req 01 - lint checked

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,22 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.queue = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.queue)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.queue.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if not self.is_empty():
+            return self.queue.pop(0)
+        else:
+            raise IndexError("Queue is empty")
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if 0 <= index < len(self.queue):
+            return self.queue[index]
+        else:
+            raise IndexError("Invalid or nonexistent index")

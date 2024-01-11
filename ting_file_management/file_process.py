1bdbb9cf193ad0dd4dd44d9cbad0bd92ff064898
joaoboscochagaspanzera <joaopanzera@hotmail.com>
2024-01-11 20:01:22 -0300

    Req 3 - lint checked

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,21 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    lines = txt_importer(path_file)
+
+    if lines is not None:
+        result = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(lines),
+            "linhas_do_arquivo": lines,
+        }
+
+    if result in instance.queue:
+        return None
+
+    instance.enqueue(result)
+    print(result)
 
 
 def remove(instance):

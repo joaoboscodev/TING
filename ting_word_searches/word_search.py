5bdd27c1d49ee86fd6d67c261ec878aa4f541260
joaoboscochagaspanzera <joaopanzera@hotmail.com>
2024-01-11 22:31:42 -0300

    Req 8 - lint checked

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,4 @@
 def exists_word(word, instance):
-    word = word.lower()
-
     result = []
 
     for file in instance.queue:
@@ -21,4 +19,23 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+
+    for file in instance.queue:
+        occurrences = [
+            {
+                "linha": num,
+                "conteudo": line.strip()
+            }
+            for num, line in enumerate(file["linhas_do_arquivo"], 1)
+            if word in line.lower()
+        ]
+
+        if occurrences:
+            result.append({
+                "palavra": word,
+                "arquivo": file["nome_do_arquivo"],
+                "ocorrencias": occurrences
+            })
+
+    return result
